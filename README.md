adafa08b66933b45ae9efc933b4f138af0414c24
Joao Tomaz <joao@truecompliance.co.uk>
2024-09-09 14:46:24 +0100

    feat: add more info

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 - [Installation](./INSTALLATION.md)
 - [Task](./TASK-DESCRIPTION.md)
+- [Package - shadcn](https://www.shadcn-vue.com/)
+- [CSS - tailwindcss](https://tailwindcss.com/)
--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
   plugins: [
     vue(),
     vueJsx(),
